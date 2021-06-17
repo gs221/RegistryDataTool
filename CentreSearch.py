9962b2d3554dba7e3754bd0b9ad6fe492fcc292e
180014469 <47386535+gs221@users.noreply.github.com>
2021-06-17 21:28:34 +0100

    Potential Demo - Safe Restore Point

--- CentreSearch.py
+++ CentreSearch.py
@@ -8,27 +8,8 @@ def centre_search(ucas: DataFrame, internal: DataFrame):
 
     # Remove records from UCAS data where corresponding school with ID is already in internal data
     removed_common = ucas[ucas['School'].isin(internal['School code']) == False]
+
+    # Get schools that now have UCAS code 
+    duplicates = internal[internal['Postcode'].isin(removed_common['Postcode'])]
     
-    # Remove UCAS codes and other malformed codes leaving only internal codes
-    without_ucas = internal[internal['School code'].str.contains(r'^[a-zA-Z]{2}\d{3,5}$', na=False)]
-
-    print(without_ucas)
-
-    # Certain matches
-    #for in_school in without_ucas['Full name']:
-    #    certain_matches = without_ucas[without_ucas['Full name'].isin(removed_common['School Name'])]
-
-    #print(certain_matches)
-
-    # Find possible matches 
-    #for in_school in without_ucas['Full name']:
-    #    for ucas_school in removed_common['School Name']:
-    #        if fuzz.ratio(in_school, ucas_school) > 70:
-    #            print(in_school, " : ", ucas_school)
-    
-    #duplicates = ucas[ucas.duplicated(subset=['School Name', 'Address line 1', 'Address line 2', 'Address line 3', 'Address line 4', 'Postcode'])]
-    
-    #print(duplicates[['School', 'School Name']].to_csv())
-
-    #existing_removed = remove_existing(ucas, internal)
-
+    print(duplicates)
